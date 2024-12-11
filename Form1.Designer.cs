db165b3acec62fa8e5775db81551b81a913a435b
burgueee <burguee16@gmail.com>
2024-12-11 18:46:22 -0300

    Miercoles 11 de diciembre del 2024

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,109 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnWord1 = new System.Windows.Forms.Button();
-            this.btnTranslate1 = new System.Windows.Forms.Button();
-            this.btnWord2 = new System.Windows.Forms.Button();
-            this.btnTranslate2 = new System.Windows.Forms.Button();
-            this.btnWord3 = new System.Windows.Forms.Button();
-            this.btnTranslate3 = new System.Windows.Forms.Button();
-            this.btnWord4 = new System.Windows.Forms.Button();
-            this.btnTranslate4 = new System.Windows.Forms.Button();
-            this.btnWord5 = new System.Windows.Forms.Button();
-            this.btnTranslate5 = new System.Windows.Forms.Button();
             this.btnContador = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnWord1
-            // 
-            this.btnWord1.Location = new System.Drawing.Point(435, 27);
-            this.btnWord1.Name = "btnWord1";
-            this.btnWord1.Size = new System.Drawing.Size(135, 55);
-            this.btnWord1.TabIndex = 0;
-            this.btnWord1.Tag = "1";
-            this.btnWord1.UseVisualStyleBackColor = true;
-            this.btnWord1.Click += new System.EventHandler(this.btnWord1_Click);
-            // 
-            // btnTranslate1
-            // 
-            this.btnTranslate1.Location = new System.Drawing.Point(294, 27);
-            this.btnTranslate1.Name = "btnTranslate1";
-            this.btnTranslate1.Size = new System.Drawing.Size(135, 55);
-            this.btnTranslate1.TabIndex = 1;
-            this.btnTranslate1.Tag = "1";
-            this.btnTranslate1.UseVisualStyleBackColor = true;
-            // 
-            // btnWord2
-            // 
-            this.btnWord2.Location = new System.Drawing.Point(435, 99);
-            this.btnWord2.Name = "btnWord2";
-            this.btnWord2.Size = new System.Drawing.Size(135, 55);
-            this.btnWord2.TabIndex = 2;
-            this.btnWord2.Tag = "2";
-            this.btnWord2.UseVisualStyleBackColor = true;
-            // 
-            // btnTranslate2
-            // 
-            this.btnTranslate2.Location = new System.Drawing.Point(294, 99);
-            this.btnTranslate2.Name = "btnTranslate2";
-            this.btnTranslate2.Size = new System.Drawing.Size(135, 55);
-            this.btnTranslate2.TabIndex = 3;
-            this.btnTranslate2.Tag = "2";
-            this.btnTranslate2.UseVisualStyleBackColor = true;
-            // 
-            // btnWord3
-            // 
-            this.btnWord3.Location = new System.Drawing.Point(435, 178);
-            this.btnWord3.Name = "btnWord3";
-            this.btnWord3.Size = new System.Drawing.Size(135, 55);
-            this.btnWord3.TabIndex = 4;
-            this.btnWord3.Tag = "3";
-            this.btnWord3.UseVisualStyleBackColor = true;
-            // 
-            // btnTranslate3
-            // 
-            this.btnTranslate3.Location = new System.Drawing.Point(294, 178);
-            this.btnTranslate3.Name = "btnTranslate3";
-            this.btnTranslate3.Size = new System.Drawing.Size(135, 55);
-            this.btnTranslate3.TabIndex = 5;
-            this.btnTranslate3.Tag = "3";
-            this.btnTranslate3.UseVisualStyleBackColor = true;
-            // 
-            // btnWord4
-            // 
-            this.btnWord4.Location = new System.Drawing.Point(435, 262);
-            this.btnWord4.Name = "btnWord4";
-            this.btnWord4.Size = new System.Drawing.Size(135, 55);
-            this.btnWord4.TabIndex = 6;
-            this.btnWord4.Tag = "4";
-            this.btnWord4.UseVisualStyleBackColor = true;
-            // 
-            // btnTranslate4
-            // 
-            this.btnTranslate4.Location = new System.Drawing.Point(294, 262);
-            this.btnTranslate4.Name = "btnTranslate4";
-            this.btnTranslate4.Size = new System.Drawing.Size(135, 55);
-            this.btnTranslate4.TabIndex = 7;
-            this.btnTranslate4.Tag = "4";
-            this.btnTranslate4.UseVisualStyleBackColor = true;
-            // 
-            // btnWord5
-            // 
-            this.btnWord5.Location = new System.Drawing.Point(435, 343);
-            this.btnWord5.Name = "btnWord5";
-            this.btnWord5.Size = new System.Drawing.Size(135, 55);
-            this.btnWord5.TabIndex = 8;
-            this.btnWord5.Tag = "5";
-            this.btnWord5.UseVisualStyleBackColor = true;
-            // 
-            // btnTranslate5
-            // 
-            this.btnTranslate5.Location = new System.Drawing.Point(294, 343);
-            this.btnTranslate5.Name = "btnTranslate5";
-            this.btnTranslate5.Size = new System.Drawing.Size(135, 55);
-            this.btnTranslate5.TabIndex = 9;
-            this.btnTranslate5.Tag = "5";
-            this.btnTranslate5.UseVisualStyleBackColor = true;
             // 
             // btnContador
             // 
@@ -146,16 +45,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 421);
             this.Controls.Add(this.btnContador);
-            this.Controls.Add(this.btnTranslate5);
-            this.Controls.Add(this.btnWord5);
-            this.Controls.Add(this.btnTranslate4);
-            this.Controls.Add(this.btnWord4);
-            this.Controls.Add(this.btnTranslate3);
-            this.Controls.Add(this.btnWord3);
-            this.Controls.Add(this.btnTranslate2);
-            this.Controls.Add(this.btnWord2);
-            this.Controls.Add(this.btnTranslate1);
-            this.Controls.Add(this.btnWord1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -164,17 +53,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnWord1;
-        private System.Windows.Forms.Button btnTranslate1;
-        private System.Windows.Forms.Button btnWord2;
-        private System.Windows.Forms.Button btnTranslate2;
-        private System.Windows.Forms.Button btnWord3;
-        private System.Windows.Forms.Button btnTranslate3;
-        private System.Windows.Forms.Button btnWord4;
-        private System.Windows.Forms.Button btnTranslate4;
-        private System.Windows.Forms.Button btnWord5;
-        private System.Windows.Forms.Button btnTranslate5;
         private System.Windows.Forms.Button btnContador;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -15,100 +15,113 @@ namespace MatchWords
         Datos datos = new Datos();
         List<Word> listaPalabras = new List<Word>();
         
+        
         private Button botonSeleccionado1 = null;
         private Button botonSeleccionado2 = null;
         private Timer timer;
         private Button botonParaOcultar1;
         private Button botonParaOcultar2;
-        private Random random;
+        private Random random = new Random();
         int indicePalabras = 0;
         int indiceTraducciones = 0;
+        Button botonPalabra = new Button();
+        Button botonTranslate = new Button();
         public Form1()
         {
             InitializeComponent();
 
-            timer = new Timer();
-            timer.Interval = 400;
-            timer.Tick += Timer_Tick;
+            //timer = new Timer();
+            //timer.Interval = 400;
+            //timer.Tick += Timer_Tick;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
-            //NO APARECEN LOS BOTONES, listaPalabras esta vacia, hay que ver por que y llenarla
-            GenerarBotonesAleatorios();
-            GenerarBotonesPrueba();
             
+
             listaPalabras = datos.listar();
             btnContador.Text = listaPalabras.Count().ToString();
+            try
+            {
+                GenerarBotonesAleatorios(listaPalabras, out botonPalabra);
+                GenerarTraduccion(listaPalabras, out botonTranslate);
+                if(botonSeleccionado2 != null) { CompararBotones(botonPalabra, botonTranslate); }
+                
+
+
+
+            }
+            catch (Exception Ex)
+            {
+
+                throw Ex;
+            }
+            //GenerarBotonesPrueba();
             ////////////////////////////////// MOSTRAR LAS PALABRAS
-            btnWord1.Text = listaPalabras.First().palabra.ToString();
-            btnTranslate1.Text = listaPalabras.First().translate.ToString();
-            btnWord2.Text = listaPalabras.ElementAt(1).palabra.ToString();
-            btnTranslate2.Text = listaPalabras.ElementAt(1).translate.ToString();
-            btnWord3.Text = listaPalabras.ElementAt(2).palabra.ToString();
-            btnTranslate3.Text = listaPalabras.ElementAt(2).translate.ToString();
-            btnWord4.Text = listaPalabras.ElementAt(3).palabra.ToString();
-            btnTranslate4.Text = listaPalabras.ElementAt(3).translate.ToString();
-            btnWord5.Text = listaPalabras.ElementAt(4).palabra.ToString();
-            btnTranslate5.Text = listaPalabras.ElementAt(4).translate.ToString();
+            //btnWord1.Text = listaPalabras.First().palabra.ToString();
+            //btnTranslate1.Text = listaPalabras.First().translate.ToString();
+            //btnWord2.Text = listaPalabras.ElementAt(1).palabra.ToString();
+            //btnTranslate2.Text = listaPalabras.ElementAt(1).translate.ToString();
+            //btnWord3.Text = listaPalabras.ElementAt(2).palabra.ToString();
+            //btnTranslate3.Text = listaPalabras.ElementAt(2).translate.ToString();
+            //btnWord4.Text = listaPalabras.ElementAt(3).palabra.ToString();
+            //btnTranslate4.Text = listaPalabras.ElementAt(3).translate.ToString();
+            //btnWord5.Text = listaPalabras.ElementAt(4).palabra.ToString();
+            //btnTranslate5.Text = listaPalabras.ElementAt(4).translate.ToString();
 
             ////////////////////////////// EVENTO DE CLICKS
 
-            btnWord1.Click += Boton_Click;
-            btnWord2.Click += Boton_Click;
-            btnWord3.Click += Boton_Click;
-            btnWord4.Click += Boton_Click;
-            btnWord5.Click += Boton_Click;
+            //btnWord1.Click += Boton_Click;
+            //btnWord2.Click += Boton_Click;
+            //btnWord3.Click += Boton_Click;
+            //btnWord4.Click += Boton_Click;
+            //btnWord5.Click += Boton_Click;
 
-            btnTranslate1.Click += Boton_Click;
-            btnTranslate2.Click += Boton_Click;
-            btnTranslate3.Click += Boton_Click;
-            btnTranslate4.Click += Boton_Click;
-            btnTranslate5.Click += Boton_Click;
+            //btnTranslate1.Click += Boton_Click;
+            //btnTranslate2.Click += Boton_Click;
+            //btnTranslate3.Click += Boton_Click;
+            //btnTranslate4.Click += Boton_Click;
+            //btnTranslate5.Click += Boton_Click;
 
             
         }
+        private void CompararBotones(Button botonPalabra, Button botonTraduccion)
+        {
+            
 
+            if (botonSeleccionado1.Tag == botonSeleccionado2.Tag)
+            {
+                MessageBox.Show("Coinciden");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("No ");
+                return;
+            }
+            
+        }
         private void Boton_Click(object sender, EventArgs e)
         {
-            Button botonActual = sender as Button;
+            Button boton = new Button();
+
+            if(boton != null) {
+                if(botonSeleccionado1 == null)
+                {
+                    botonSeleccionado1 = boton;
+                    boton.BackColor = Color.LightBlue;
+                }
+                else if(botonSeleccionado2 == null)
+                {
+                    botonSeleccionado2 = boton;
+                    boton.BackColor = Color.LightBlue;
+
+                    CompararBotones(botonPalabra, botonTranslate);
+                    return;
+                }
+            }
             
-            if(botonActual == null)
-            {
-                MessageBox.Show("ERROR");
-                return;
-            }
-            if(botonSeleccionado1 == null)
-            {
-                botonSeleccionado1 = botonActual;
-                botonSeleccionado1.BackColor = Color.LightBlue;
-                return;
-            }
-            if(botonSeleccionado2 == null)
-            {
-                botonSeleccionado2 = botonActual;
-                botonSeleccionado2.BackColor = Color.LightBlue;
-
-                if(botonSeleccionado1.Tag != null && botonSeleccionado2.Tag != null &&
-                    botonSeleccionado1.Tag.ToString() ==  botonSeleccionado2.Tag.ToString())
-                {
-                    botonSeleccionado1.BackColor = Color.Green;
-                    botonSeleccionado2.BackColor = Color.Green;
-
-                    botonParaOcultar1 = botonSeleccionado1;
-                    botonParaOcultar2 = botonSeleccionado2;
-
-                    timer.Start();
-                }
-                else
-                {
-                    botonSeleccionado1.BackColor = Color.Red;
-                    botonSeleccionado2.BackColor = Color.Red;
-                }
-                botonSeleccionado1 = null;
-                botonSeleccionado2 = null;
-                return;
-            }
+            
         }
         private void Timer_Tick(object sender,EventArgs e)
         {
@@ -119,7 +132,7 @@ namespace MatchWords
                 
                 try
                 {
-                    GenerarPalabra(botonParaOcultar1,listaPalabras,ref indicePalabras);
+                    //GenerarPalabra(botonParaOcultar1,listaPalabras,ref indicePalabras);
                     
                 }
                 catch (Exception ex)
@@ -136,7 +149,7 @@ namespace MatchWords
                 
                 try
                 {
-                    GenerarTraduccion(botonParaOcultar2, listaPalabras, ref indiceTraducciones);
+                    //GenerarTraduccion(botonParaOcultar2, listaPalabras, ref indiceTraducciones);
                 }
                 catch (Exception ex)
                 {
@@ -154,63 +167,56 @@ namespace MatchWords
             botonParaOcultar2 = null;
 
         }
-        private void GenerarPalabra(Button botonParaOcultar,List<Word> listaPalabras, ref int indicePalabras)
+        
+        public void GenerarTraduccion(List<Word> translates, out Button botonTranslate)
         {
-            
-            if(indicePalabras >= listaPalabras.Count)
+            botonTranslate = null;
+            translates = datos.listar();
+            List<Point> posicionesTranslates = new List<Point>
             {
-                MessageBox.Show("No hay mas palabras");
-                return;
-            }
-
-            Size sizeBoton = botonParaOcultar.Size;
-            Point positionBoton = botonParaOcultar.Location;
-
-            this.Controls.Remove(botonParaOcultar);
-
-            string nuevaPalabra = listaPalabras[indicePalabras].palabra;
-            Button nuevoBoton = new Button
-            {
-                Location = positionBoton,
-                Size = sizeBoton,
-                Tag = listaPalabras[indicePalabras],
-                Text = nuevaPalabra
+                new Point (294,27),
+                new Point (294,99),
+                new Point (294,178),
+                new Point (294,262),
+                new Point (294,343),
             };
-            nuevoBoton.Click += Boton_Click;
 
-            this.Controls.Add(nuevoBoton);
-            indicePalabras++;
-
-        }
-        private void GenerarTraduccion(Button botonParaOcultar, List<Word> listaPalabras, ref int indiceTraducciones)
-        {
-            if (indiceTraducciones >= listaPalabras.Count)
-            {
-                MessageBox.Show("No hay mas palabras");
-                return;
+            if (translates == null || translates.Count == 0) {
+                MessageBox.Show("La lista de traducciones está vacía o no se ha inicializado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
             }
+            List<int> indicesUsados = new List<int>();
 
-            Size sizeBoton = botonParaOcultar.Size;
-            Point positionBoton = botonParaOcultar.Location;
-
-            this.Controls.Remove(botonParaOcultar);
-
-            string nuevaPalabra = listaPalabras[indiceTraducciones].translate;
-            Button nuevoBoton = new Button
+            foreach(var posicion in posicionesTranslates)
             {
-                Location = positionBoton,
-                Size = sizeBoton,
-                Tag = listaPalabras[indiceTraducciones],
-                Text = nuevaPalabra
-            };
-            nuevoBoton.Click += Boton_Click;
+                if (posicionesTranslates.Count == 0) break;
+                int indice;
+                do
+                {
+                    indice= random.Next(translates.Count);
 
-            this.Controls.Add(nuevoBoton);
-            indiceTraducciones++;
+                } while (indicesUsados.Contains(indice));
+                indicesUsados.Add(indice);
+                
+                
 
+                 botonTranslate = new Button
+                {
+                    Text = translates[indice].translate,
+                    Size = new Size(135, 55),
+                    Location = posicion,
+                    Tag = translates[indice].Id,
+                    
+                };
+                botonTranslate.Click += Boton_Click;
+                botonTranslate.Tag = (int)botonTranslate.Tag +1;
+                this.Controls.Add(botonTranslate);
+            }
         }
-        private void GenerarBotonesAleatorios()
+        public void GenerarBotonesAleatorios(List<Word> palabras, out Button botonPalabra)
         {
+            botonPalabra = null;
+            palabras = datos.listar();
             List<Point> posicionesPalabras = new List<Point>
             {
                 new Point (48,27),
@@ -220,49 +226,41 @@ namespace MatchWords
                 new Point (48,343),
             };
 
-            List<Point> posicionesTraducciones = new List<Point>
-            {
-                new Point (294,27),
-                new Point (294,99),
-                new Point (294,178),
-                new Point (294,262),
-                new Point (294,343),
-            };
-
             //string nuevaPalabra = listaPalabras[indicePalabras].palabra;
-            if (listaPalabras == null || listaPalabras.Count == 0)
+            if (palabras == null || palabras.Count == 0)
             {
                 MessageBox.Show("La lista de palabras está vacía o no se ha inicializado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            foreach (var palabra in listaPalabras)
+            List<int> indicesUsados = new List<int>();
+
+            foreach (var posicion in posicionesPalabras)
             {
                 if (posicionesPalabras.Count == 0) break;
 
-                int indice = random.Next(posicionesPalabras.Count);
-                Point posicion = posicionesPalabras[indice];
-                posicionesPalabras.RemoveAt(indice);
-
-                Button botonPalabra = new Button
+                int indice;
+                do
                 {
-                    Text = listaPalabras[indice].palabra,
+                    indice = random.Next(palabras.Count);
+                } while (indicesUsados.Contains(indice));
+                indicesUsados.Add(indice);
+                
+                
+
+                 botonPalabra = new Button
+                {
+                    Text = palabras[indice].palabra,
                     Size = new Size(135,55),
-                    Location = posicion
+                    Location = posicion,
+                    Tag = palabras[indice].Id,
                 };
+                botonPalabra.Tag = (int)botonPalabra.Tag +1;
                 botonPalabra.Click += Boton_Click;
+                
                 this.Controls.Add(botonPalabra);
             }
         }
-        private void GenerarBotonesPrueba()
-        {
-            Button testButton = new Button
-            {
-                Text = "Prueba",
-                Size = new Size(135, 55),
-                Location = new Point(50, 50) // Posición fija de prueba
-            };
-            this.Controls.Add(testButton);
-        }
+        
         private void btnWord1_Click(object sender, EventArgs e)
         {
 
